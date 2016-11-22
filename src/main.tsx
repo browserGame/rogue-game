17b892512089a48e27716066e5eb0283af8a2f59
jacob bogers <jkfbogers@gmail.cim>
2016-11-22 12:48:21 +0100

    inital scaffolding and setup

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+/// <reference path="../typings/react/react-global.d.ts" />
+
+function hello(compiler: string) {
+    console.log(`Hello from ${compiler}`);
+	return <a>Hello this is a test in JSX stuff</a>;
+}
+hello("TypeScript");
+
+
+
